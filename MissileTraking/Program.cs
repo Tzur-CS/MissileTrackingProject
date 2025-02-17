384f22ac637286feac0dea5f4acdbf11af1a89ce
Tzur-CS <105347894+Tzur-CS@users.noreply.github.com>
2025-02-17 17:10:33 +0200

    reorganized class

--- MissileTraking/Program.cs
+++ MissileTraking/Program.cs
@@ -8,6 +8,18 @@ namespace MissileTracking
     {
         public static async Task Main(string[] args)
         {
+            var options = DbSetUp();
+
+            Func<MissileDbContext> dbContextProvider = () => new MissileDbContext(options);
+            var trackingService = new MissileTrackingService(dbContextProvider, ["CityA", "CityB"]);
+
+            var server = new TcpConnectionService(trackingService);
+
+            await server.StartAsync();
+        }
+
+        private static DbContextOptions<MissileDbContext> DbSetUp()
+        {
             var options = new DbContextOptionsBuilder<MissileDbContext>()
                 .UseSqlite("Data Source=missiles.db")
                 .Options;
@@ -17,18 +29,10 @@ namespace MissileTracking
                 File.Delete("missiles.db");
             }
 
-            using (var context = new MissileDbContext(options))
-            {
-                context.Database.EnsureCreated();
-            }
+            using var context = new MissileDbContext(options);
+            context.Database.EnsureCreated();
 
-            Func<MissileDbContext> dbContextProvider = () => new MissileDbContext(options);
-            HashSet<string> interceptionPolicy = new HashSet<string> { "CityA", "CityB" };
-
-            var trackingService = new MissileTrackingService(dbContextProvider, interceptionPolicy);
-            var server = new TcpConnectionService(5000, trackingService);
-
-            await server.StartAsync();
+            return options;
         }
     }
 }
